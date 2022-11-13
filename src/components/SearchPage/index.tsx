e32715c41029f361fa0f28deb5899caf65ab36a6
dragon <1183665002@qq.com>
2022-11-14 07:50:52 +0800

    添加search页面

---
+++ src/components/SearchPage/index.tsx
@@ -0,0 +1,20 @@
+import { FC, useEffect, useState } from 'react'
+import { View, Text, CoverImage } from '@tarojs/components'
+import Carousel from '../Carousel';
+import CardMsgList from '../CardMsgList';
+import { rotation, emerge, product } from "../../config/taroApi";
+import './index.scss'
+
+
+const SearchPage: FC = () => {
+
+    return (
+        <>
+            <View>
+                <Text>SearchPage</Text>
+            </View>
+        </>
+    )
+}
+
+export default SearchPage
